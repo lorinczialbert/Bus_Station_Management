124676c4a6a477255f3bc8fce9892146b6d2e7d7
Albert Lorinczi <88albertlorinczi@gmail.com>
2025-10-27 14:41:47 +0200

    Initial commit

---
+++ busstation/src/main/java/com/example/busstation/BusstationApplication.java
@@ -0,0 +1,13 @@
+package com.example.busstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusstationApplication.class, args);
+	}
+
+}
---
+++ busstation/src/test/java/com/example/busstation/BusstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.busstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
